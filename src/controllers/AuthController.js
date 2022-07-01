11f5a5dec2e43cd89cca852f4f6ee8926bcfec70
Jheyele Raquel <jheyele.xavier@atos.net>
2022-06-30 22:06:15 -0300

    Add validation

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -5,28 +5,31 @@ import jwt from 'jsonwebtoken';
 export default class AuthController {
     async authenticate(req, res) {
         const { email, password } = req.body;
+        try {
+            const userExist = await prismaClient.user.findFirst({ where: { email } });
 
-        const userExist = await prismaClient.user.findFirst({ where: { email } });
+            if (!userExist) {
+                return res.status(401).json("Authentication failure");
+            }
 
-        if (!userExist) {
-            return res.status(401).json("Authentication failure");
+            const isValidPassword = await bcrypt.compare(password, userExist.password);
+
+            if (!isValidPassword) {
+                return res.status(401).json("Authentication failure");
+            }
+
+            const token = jwt.sign({ id: userExist.id }, process.env.SECRET_KEY_JWT, { expiresIn: '2h' });
+
+            const user = {
+                "id": userExist.id,
+                "name": userExist.name
+            }
+            return res.json({
+                user,
+                token,
+            });
+        } catch (err) {
+            return res.status(400).json("Invalid data");
         }
-
-        const isValidPassword = await bcrypt.compare(password, userExist.password);
-
-        if (!isValidPassword) {
-            return res.status(401).json("Authentication failure");
-        }
-
-        const token = jwt.sign({ id: userExist.id }, process.env.SECRET_KEY_JWT, { expiresIn: '2h' });
-
-        const user = {
-            "id": userExist.id,
-            "name": userExist.name
-        }
-        return res.json({
-            user,
-            token,
-        });
     }
 }
